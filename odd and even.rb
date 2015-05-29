07fe4f3d7906825642f9978d3071fb2cd4e89f04
农民也很疯狂 <qluoluo@icloud.com>
2015-05-29 17:48:45 -0500

    Update odd and even.rb

--- odd and even.rb
+++ odd and even.rb
@@ -1,21 +1,31 @@
-module Rule
-	def startgame
-		number = @number
-	 if number.to_i % 2 == 0
-	 	puts "#{number} is odd"
-	 else
-	 	puts "#{number} is even"
-	 end
+module Gamerules
+	def game(number=@number)
+		if number.to_i % 2 == 0
+			puts "#{number} is a odd"
+		else
+			puts "#{number} is a even"
+		end
 	end
 end
-
 
 class NumberGame
 	attr_accessor :number
-	def iniatilize(number)
-		number = @number
+	include Gamerules
+	def initialize(number)
+		@number = number
+		puts @number
 	end
 end
 
-NumberGame.new
-NumberGame.startgame
+number = NumberGame.new(2)
+number.game
+10.times do print "==========" end
+number.game(232)
+10.times do print "==========" end
+number.game(243)
+110.times do print "==========" end
+number.game(233)
+10.times do print "==========" end
+number.game(231)
+10.times do print "==========" end
+number.game(232)
